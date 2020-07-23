6e9e6d43116b51dc93d60af320b3c6c735afab98
tomas <tomas_053@hotmail.es>
2020-07-23 17:57:24 -0300

    colino

--- js/countdown/jquery.downCount.js
+++ js/countdown/jquery.downCount.js
@@ -18,7 +18,7 @@
 
         // Throw error if date is set incorectly
         if (!Date.parse(settings.date)) {
-            $.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
+            $.error('Incorrect date format, it should look like this, 12/24/2020 12:00:00.');
         }
 
         // Save container
